247c73fe55ce6f227aeede61a4fd537480586d1f
Noxdor <lennart@guderian.de>
2023-05-15 18:06:58 +0200

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "plugin:vue/vue3-recommended",
+  overrides: [],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {},
+};
